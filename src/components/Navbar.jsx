52d83a32358fb7cd01a308de95b1d8585949bee0
Natalie Gillam <ngillam@hotmail.co.uk>
2023-06-30 11:21:35 +0100

    html for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <Nav>
+        <div className="brand">
+          <div className="container">
+            <img src={logo} alt="logo" />
+            Travelo
+          </div>
+          <div className="toggle">
 
-export default Navbar
+          </div>
+        </div>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#recommend">Places</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+        </ul>
+        <button>Connect</button>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
+
+const Nav = styled.nav`
+
+`;
