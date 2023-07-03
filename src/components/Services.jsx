bdcb3101d9b24bfaa4f58857c42a549475a35e07
Natalie Gillam <ngillam@hotmail.co.uk>
2023-07-03 09:14:29 +0100

    CSS for services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -57,5 +57,26 @@ const Services = () => {
 export default Services;
 
 const Section = styled.section`
-
+padding: 5rem 0;
+display: grid;
+grid-template-columns: repeat(4, 1fr);
+gap: 1rem;
+.service{
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem;
+  background-color: aliceblue;
+  box-shadow: rgba(100, 111, 111, 0.2) 0 7px 29px 0;
+  transition: 0.3s ease-in-out;
+  &:hover {
+    transform: translate(0.4rem, -1rem);
+    box-shadow: rgba(0,0,0,0.35) 0px 5px 15px;
+  }
+  .icon{
+    img {
+      height: 2.4rem;
+    }
+  }
+}
 `
