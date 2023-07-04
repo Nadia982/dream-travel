cec82f4593bb18139ffa28e757c3fea998dfaa76
Natalie Gillam <ngillam@hotmail.co.uk>
2023-07-04 22:47:14 +0100

    Added content and structure for Recommend section

--- src/components/Recommend.jsx
+++ src/components/Recommend.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Destination1 from "../assets/Destination1.png";
 import Destination2 from "../assets/Destination2.png";
 import Destination3 from "../assets/Destination3.png";
 import Destination4 from "../assets/Destination4.png";
 import Destination5 from "../assets/Destination5.png";
-import Destination6 from "../assets/Destination6.png";
+import Destination6 from "../assets/pexels-bill-emrich-230794.jpg";
 import info1 from "../assets/info1.png";
 import info2 from "../assets/info2.png";
 import info3 from "../assets/info3.png";
@@ -17,52 +17,95 @@ const Recommend = () => {
       title: "Singapore",
       subTitle:
         "Discover Singapore's captivating blend of opulence, innovation, and cultural splendor, where luxury meets tradition in a vibrant urban oasis.",
-      duration: "7 nights"
+      duration: "7 nights",
     },
     {
       image: Destination2,
       title: "Thailand",
-      subTitle: "Indulge in Thailand's opulent treasures, from ornate temples and immaculate beaches to world-class resorts and Michelin-starred cuisine.",
-      duration: "14 nights"
+      subTitle:
+        "Indulge in Thailand's opulent treasures, from ornate temples and immaculate beaches to world-class resorts and Michelin-starred cuisine.",
+      duration: "14 nights",
     },
     {
       image: Destination3,
       title: "Paris",
-      subTitle: "Experience the epitome of elegance and sophistication in the enchanting city of Paris, where world-class cuisine, iconic landmarks, and unparalleled artistry await.",
-      duration: "3 nights"
+      subTitle:
+        "Experience the epitome of elegance and sophistication in the enchanting city of Paris, where world-class cuisine, iconic landmarks, and unparalleled artistry await.",
+      duration: "3 nights",
     },
     {
       image: Destination4,
       title: "New Zealand",
-      subTitle: "Discover a pristine oasis of breathtaking landscapes, untouched wilderness, and world-class amenities in New Zealand.",
-      duration: "14 nights"
+      subTitle:
+        "Discover a pristine oasis of breathtaking landscapes, untouched wilderness, and world-class amenities in New Zealand.",
+      duration: "14 nights",
     },
     {
       image: Destination5,
       title: "Bora Bora",
-      subTitle: "Experience ultimate luxury at Bora Bora, where flawless turquoise waters, overwater bungalows, and world-class amenities create an unforgettable tropical escape.",
-      duration: "14 nights"
+      subTitle:
+        "Experience ultimate luxury at Bora Bora, where flawless turquoise waters, overwater bungalows, and world-class amenities create an unforgettable tropical escape.",
+      duration: "14 nights",
     },
     {
       image: Destination6,
       title: "London",
-      subTitle: "Immerse yourself in the timeless elegance and vibrant energy of London, a city where rich history meets modern luxury.",
-      duration: "7 nights"
+      subTitle:
+        "Immerse yourself in the timeless elegance and vibrant energy of London, a city where rich history meets modern luxury.",
+      duration: "7 nights",
     },
   ];
 
   const packages = [
-    "The Weekend Break", 
+    "The Weekend Break",
     "The Package Holiday",
     "The Group Tour",
-    "Long Term Slow Travel"
-  ]
+    "Long Term Slow Travel",
+  ];
 
-const [active, setActive] = useState(1);
+  const [active, setActive] = useState(1);
 
-  return <Section id="recommend">
-   
-  </Section>;
+  return (
+    <Section id="recommend">
+      <div className="title">
+        <h2>Recommended destinations</h2>
+      </div>
+      <div className="packages">
+        <ul>
+          {packages.map((pkg, index) => {
+            return (
+              <li
+                className={active === index + 1 ? "active" : ""}
+                onClick={() => setActive(index + 1)}
+              >
+                {pkg}
+              </li>
+            );
+          })}
+        </ul>
+        <div className="destinations">
+          {data.map((destination)=> {
+            return (
+              <div className="derst">
+                <img src={destination.image} alt={destination.title} />
+                <h3>{destination.title}</h3>
+                <p>{destination.subTitle}</p>
+                <div className="info">
+                  <div className="services">
+                    <img src={info1} alt="logo" />
+                    <img src={info2} alt="logo" />
+                    <img src={info3} alt="logo" />
+                  </div>
+                  <h4>{destination.duration}</h4>
+                </div>
+              </div>
+            )
+          }
+          )}
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Recommend;
