e203b39fba8241ed0d36c487ebd00bcb2dd8f6ac
Natalie Gillam <ngillam@hotmail.co.uk>
2023-07-06 00:50:20 +0100

    Content and structure for Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { BsLinkedin, BsFacebook } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <FooterContainer>
+      <ul className="links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#recommend">Recommend</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+      </ul>
+      <ul className="social__links">
+       <li><BsFacebook/></li>
+       <li><BsLinkedin/></li>
+       <li><AiFillInstagram/></li>
+      </ul>
+    </FooterContainer>
+  );
+};
 
-export default Footer
+export default Footer;
+
+const FooterContainer = styled.footer``;
